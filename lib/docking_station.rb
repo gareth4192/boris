c30daeecb1e7952e577d063af6d527b904151d61
gareth4192 <gareth-123@live.com>
2015-11-25 16:59:01 +0000

    challange 15 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,33 @@
 require_relative 'bike'
 
 class DockingStation
-	attr_reader :bike
 
-	def release_bike
-		raise 'no bikes' unless @bike
-		@bike
+	attr_reader :bikes
+
+	def initialize
+		@bikes = []
 	end
 
+
+	def release_bike
+		raise 'no bikes' if empty?
+		@bikes.pop
+	end
 
 
 	def dock_bike(bike)
-		@bike = bike
+		raise 'station full' if full?
+		@bikes << bike
 	end
+
+private 
+	def full?
+	@bikes.size >= 20 ? true : false
+	end
+
+	def empty?
+		@bikes.empty? ? true : false
+	end
+
 
 end
